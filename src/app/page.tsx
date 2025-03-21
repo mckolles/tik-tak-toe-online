e952ebe0d9f3607e7933a967963b4c9654509176
Nikita Kolesnikov <koless98@icloud.com>
2025-03-21 10:44:01 +0300

    Day 2: added DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+
+  const games = await prisma.game.findMany()
+  console.log(games)
   return (
-    <Button size={'lg'} variant={'destructive'}> Hellow </Button>
+    <>
+      <Button size={'lg'} variant={'destructive'}>Hello</Button>
+      {games.map(game => (
+        <Card key={game.id}>
+          <CardTitle>{game.name}</CardTitle>
+        </Card>
+      ))}
+    </>
   );
 }
+
  
