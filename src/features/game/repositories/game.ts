ed0d24220b6cb5b8eebebf079d712587e226bff2
Nikita Kolesnikov <koless98@icloud.com>
2025-03-31 09:21:44 +0300

    Day 5: Added game

--- src/features/game/repositories/game.ts
+++ src/features/game/repositories/game.ts
@@ -1,7 +1,25 @@
-import { GameEntity } from "../domain";
+import { prisma } from "@/shared/lib/db";
+import { Game, User } from "@prisma/client";
 
-export const gameRepository = {
-    gamesList: (): Promise<GameEntity[]> => {
-        
-    },
+
+async function gameList(): Promise<GameEntity[]> {
+    const games = await prisma.game.findMany({
+        include: {
+            winner: true,
+            players: true,
+        },
+    });
 }
+
+function dbGameToGameEntity(game: Game & {
+    players: User[]
+}): GameEntity {
+   switch (game.status) {
+       case "idle":
+   }
+}
+    
+
+
+
+export const gameRepository = { gameList }
